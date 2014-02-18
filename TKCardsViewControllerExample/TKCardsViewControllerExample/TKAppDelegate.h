c382c73507d5b659473ab2fbe1b2bfbdd7949dc9
Tim Kersey <t@k-t.im>
2014-02-17 16:48:02 -0800

    Add empty project to be used a an example

---
+++ TKCardsViewControllerExample/TKCardsViewControllerExample/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  TKCardsViewControllerExample
+//
+//  Created by Tim Kersey on 2/17/14.
+//  Copyright (c) 2014 Tim Kersey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TKCardsViewControllerExample/TKCardsViewControllerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TKCardsViewControllerExample
+//
+//  Created by Tim Kersey on 2/17/14.
+//  Copyright (c) 2014 Tim Kersey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKAppDelegate class]));
+    }
+}
